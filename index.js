f4e6f0269e17293d5a66d4bee3e45a14a941bf85
Chimaobi-Wisdom <118293566+Chimaobi-Wisdom@users.noreply.github.com>
2023-02-07 18:22:55 +0100

    third commit

---
+++ index.js
@@ -0,0 +1,9 @@
+/*var half-circle = document.getElementById('half-circle-1');
+
+function myFunc(){
+    if(half-circle.style.display == 'block'){
+        half-circle.style.display = 'none'
+    } else {
+        half-circle.style.display = 'block'
+    }
+}*/
